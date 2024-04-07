e894113449e95efe627e996d53b9e9ba387fa73b
jihyunniiii <jita1125@naver.com>
2024-04-07 13:37:25 +0900

    [feat] #1 core-ui module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,8 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.telepigeon.android.library)
+}
+
+android {
+    namespace="com.dongguk.telepigeon.core.ui"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "TelePigeon"
 include(":app")
+include(":core:ui")
