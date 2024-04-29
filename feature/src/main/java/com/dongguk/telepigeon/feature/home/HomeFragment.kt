ae2027d4c88c3c0f1fed777e653951165a14d82c
jihyunniiii <jita1125@naver.com>
2024-04-29 11:06:31 +0900

    [feat] #7 fragmentHome

---
+++ feature/src/main/java/com/dongguk/telepigeon/feature/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.dongguk.telepigeon.feature.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.dongguk.telepigeon.feature.R
+import com.dongguk.telepigeon.feature.databinding.FragmentHomeBinding
+import com.dongguk.telpigeon.core.ui.base.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>({ FragmentHomeBinding.inflate(it) }) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvHomeHome.setOnClickListener {
+            navigateToMain()
+        }
+    }
+
+    private fun navigateToMain() {
+        findNavController().navigate(R.id.action_home_to_main)
+    }
+}
