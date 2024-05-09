a0f14e8be3e688eeeb1c0b3140b7b486ae2af3d3
jihyunniiii <jita1125@naver.com>
2024-05-09 17:33:59 +0900

    [feat] #11 QnaFragment UI 구현

---
+++ feature/src/main/java/com/dongguk/telepigeon/feature/main/qna/QnaFragment.kt
@@ -0,0 +1,7 @@
+package com.dongguk.telepigeon.feature.main.qna
+
+import com.dongguk.telepigeon.feature.databinding.FragmentQnaBinding
+import com.dongguk.telpigeon.core.ui.base.BindingFragment
+
+class QnaFragment : BindingFragment<FragmentQnaBinding>({ FragmentQnaBinding.inflate(it) }) {
+}
