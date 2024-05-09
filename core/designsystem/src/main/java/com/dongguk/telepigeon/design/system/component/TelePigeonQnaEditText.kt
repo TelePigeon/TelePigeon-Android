812c457142c844fec5809fe9f262e9fe231c0fa7
jihyunniiii <jita1125@naver.com>
2024-05-09 17:33:38 +0900

    [feat] #11 feat TelePigeonQnaEditText

---
+++ core/designsystem/src/main/java/com/dongguk/telepigeon/design/system/component/TelePigeonQnaEditText.kt
@@ -0,0 +1,46 @@
+package com.dongguk.telepigeon.design.system.component
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.dongguk.telepigeon.core.design.system.R
+import com.dongguk.telepigeon.core.design.system.databinding.EditTextQnaTelepigeonBinding
+import com.dongguk.telepigeon.design.system.type.TelePigeonQnaEditTextType
+
+@SuppressLint("CustomViewStyleable")
+class TelePigeonQnaEditText
+@JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding: EditTextQnaTelepigeonBinding
+    val editText get() = binding.etEditTextQnaTelepigeon
+
+    init {
+        binding = EditTextQnaTelepigeonBinding.inflate(LayoutInflater.from(context), this, true)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TelePigeonQnaEditText)
+        try {
+            initView(typedArray)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    private fun initView(typedArray: TypedArray) {
+        typedArray.apply {
+            with(binding) {
+                etEditTextQnaTelepigeon.hint = getString(R.styleable.TelePigeonEditText_telePigeonEditTextHint)
+            }
+        }
+    }
+
+    fun initLayout(
+        qnaEditTextType: TelePigeonQnaEditTextType
+    ) {
+        binding.ivEditTextQnaTelepigeon.setImageDrawable(context.getDrawable(qnaEditTextType.icon))
+    }
+}
