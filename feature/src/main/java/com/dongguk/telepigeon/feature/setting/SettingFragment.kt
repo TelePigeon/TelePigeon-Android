ac858928ebea3036b48bd780380895618fd62dae
jihyunniiii <jita1125@naver.com>
2024-05-29 14:22:37 +0900

    [feat] #13 초대 코드 복사 기능 구현

--- feature/src/main/java/com/dongguk/telepigeon/feature/setting/SettingFragment.kt
+++ feature/src/main/java/com/dongguk/telepigeon/feature/setting/SettingFragment.kt
@@ -1,8 +1,13 @@
 package com.dongguk.telepigeon.feature.setting
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import com.dongguk.telepigeon.design.system.component.BottomSheetWithOneBtnDialogFragment
+import com.dongguk.telepigeon.design.system.type.BottomSheetWithOneBtnType
 import com.dongguk.telepigeon.feature.databinding.FragmentSettingBinding
 import com.dongguk.telpigeon.core.ui.base.BindingFragment
 
@@ -18,6 +23,7 @@ class SettingFragment : BindingFragment<FragmentSettingBinding>({ FragmentSettin
 
         initAdapter()
         initLayout()
+        setBtnSettingRoomInfoCopyClickListener()
     }
 
     private fun initAdapter() {
@@ -43,5 +49,29 @@ class SettingFragment : BindingFragment<FragmentSettingBinding>({ FragmentSettin
             tvSettingKeyWordAgeGroupContent.text = settingViewModel.dummyRoomKeywordExtraModel.ageRange
             tvSettingKeyWordRelationshipContent.text = settingViewModel.dummyRoomKeywordExtraModel.relation
         }
+    }
+
+    private fun setBtnSettingRoomInfoCopyClickListener() {
+        binding.btnSettingRoomInfoCopy.setOnClickListener {
+            copyCopyCode(settingViewModel.dummyRoomInfoModel.code)
+            showCopyCodeBottomSheetDialogFragment()
+        }
+    }
+
+    private fun showCopyCodeBottomSheetDialogFragment() {
+        BottomSheetWithOneBtnDialogFragment(
+            bottomSheetWithOneBtnType = BottomSheetWithOneBtnType.COPY_CODE
+        ).show(childFragmentManager, COPY_CODE_BOTTOM_SHEET)
+    }
+
+    private fun copyCopyCode(code: String) {
+        (requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).let { clipboardManager ->
+            clipboardManager.setPrimaryClip(ClipData.newPlainText(CODE, code))
+        }
+    }
+
+    companion object {
+        private const val COPY_CODE_BOTTOM_SHEET = "copyCodeBottomSheet"
+        private const val CODE = "code"
     }
 }
