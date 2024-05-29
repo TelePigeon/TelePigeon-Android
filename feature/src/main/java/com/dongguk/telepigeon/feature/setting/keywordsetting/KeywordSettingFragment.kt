b2e828a32a3bb61982a8c2168d317f08f3b66f75
jihyunniiii <jita1125@naver.com>
2024-05-29 14:40:36 +0900

    [feat] #13 방 설정 -> 키워드 설정 뷰 연결

---
+++ feature/src/main/java/com/dongguk/telepigeon/feature/setting/keywordsetting/KeywordSettingFragment.kt
@@ -0,0 +1,7 @@
+package com.dongguk.telepigeon.feature.setting.keywordsetting
+
+import com.dongguk.telepigeon.feature.databinding.FragmentKeywordSettingBinding
+import com.dongguk.telpigeon.core.ui.base.BindingFragment
+
+class KeywordSettingFragment: BindingFragment<FragmentKeywordSettingBinding>({ FragmentKeywordSettingBinding.inflate(it)}) {
+}
