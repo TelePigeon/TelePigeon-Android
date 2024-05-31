935d1278d38bfc04bbf36880ee5ca1dbd97887b1
jihyunniiii <jita1125@naver.com>
2024-05-31 10:15:39 +0900

    [feat] #13 시간 설정 바텀시트 구현

---
+++ core/designsystem/src/main/java/com/dongguk/telepigeon/design/system/component/BottomSheetTimeDialogFragment.kt
@@ -0,0 +1,65 @@
+package com.dongguk.telepigeon.design.system.component
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import com.dongguk.telepigeon.core.design.system.R
+import com.dongguk.telepigeon.core.design.system.databinding.DialogBottomSheetTimeBinding
+import com.dongguk.telepigeon.design.system.type.TimeType
+import com.dongguk.telpigeon.core.ui.base.BindingBottomSheetDialogFragment
+import com.dongguk.telpigeon.core.ui.util.fragment.stringOf
+import com.google.android.material.chip.Chip
+
+class BottomSheetTimeDialogFragment(
+    private val clickBtn: () -> Unit = {},
+    private val onDialogClosed: (String) -> Unit = {},
+) :BindingBottomSheetDialogFragment<DialogBottomSheetTimeBinding>({ DialogBottomSheetTimeBinding.inflate(it)}) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setBtnBottomSheetWithOneBtnClickListeners()
+        setTimeChip()
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        onDialogClosed(getSelectedTimeChip())
+    }
+
+    private fun setBtnBottomSheetWithOneBtnClickListeners() {
+        binding.btnBottomSheetTimeBtn.setOnClickListener {
+            clickBtn()
+            dismiss()
+        }
+    }
+
+    private fun setTimeChip() {
+        binding.cgBottomSheetTime.removeAllViews()
+
+        for (time in TimeType.entries) {
+            (layoutInflater.inflate(R.layout.view_keyword_chip, binding.cgBottomSheetTime, false) as Chip).run {
+                text = stringOf(time.timeRes)
+                binding.cgBottomSheetTime.addView(this)
+            }
+        }
+    }
+
+    private fun getSelectedTimeChip(): String {
+        var times = TIME
+
+        with(binding.cgBottomSheetTime) {
+            checkedChipIds.forEach { chipId ->
+                times += findViewById<Chip>(chipId).text
+            }
+        }
+
+        return times
+    }
+
+    companion object {
+        const val TIME = "시간"
+    }
+}
---
+++ core/designsystem/src/main/java/com/dongguk/telepigeon/design/system/type/TimeType.kt
@@ -0,0 +1,81 @@
+package com.dongguk.telepigeon.design.system.type
+
+import androidx.annotation.StringRes
+import com.dongguk.telepigeon.core.design.system.R
+
+enum class TimeType(
+    @StringRes val timeRes: Int
+) {
+    TIME_01(
+        timeRes = R.string.time_1
+    ),
+    TIME_02(
+        timeRes = R.string.time_2
+    ),
+    TIME_03(
+        timeRes = R.string.time_3
+    ),
+    TIME_04(
+        timeRes = R.string.time_4
+    ),
+    TIME_05(
+        timeRes = R.string.time_5
+    ),
+    TIME_06(
+        timeRes = R.string.time_6
+    ),
+    TIME_07(
+        timeRes = R.string.time_7
+    ),
+    TIME_08(
+        timeRes = R.string.time_8
+    ),
+    TIME_09(
+        timeRes = R.string.time_9
+    ),
+    TIME_10(
+        timeRes = R.string.time_10
+    ),
+    TIME_11(
+        timeRes = R.string.time_11
+    ),
+    TIME_12(
+        timeRes = R.string.time_12
+    ),
+    TIME_13(
+        timeRes = R.string.time_13
+    ),
+    TIME_14(
+        timeRes = R.string.time_14
+    ),
+    TIME_15(
+        timeRes = R.string.time_15
+    ),
+    TIME_16(
+        timeRes = R.string.time_16
+    ),
+    TIME_17(
+        timeRes = R.string.time_17
+    ),
+    TIME_18(
+        timeRes = R.string.time_18
+    ),
+    TIME_19(
+        timeRes = R.string.time_19
+    ),
+    TIME_20(
+        timeRes = R.string.time_20
+    ),
+    TIME_21(
+        timeRes = R.string.time_21
+    ),
+    TIME_22(
+        timeRes = R.string.time_22
+    ),
+    TIME_23(
+        timeRes = R.string.time_23
+    ),
+    TIME_24(
+        timeRes = R.string.time_24
+    ),
+}
