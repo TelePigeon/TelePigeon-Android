e576060833a72f9f756d2dbb59247aa5f658b2da
jihyunniiii <jita1125@naver.com>
2024-06-02 23:21:14 +0900

    [feat] #15 월간 리포트 뷰 구현

--- feature/src/main/java/com/dongguk/telepigeon/feature/calendar/calendar/CalendarViewModel.kt
+++ feature/src/main/java/com/dongguk/telepigeon/feature/calendar/calendar/CalendarViewModel.kt
@@ -7,21 +7,20 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CalendarViewModel @Inject constructor() : ViewModel() {
-    val dummyQuestionAnswerModels = emptyList<QuestionAnswerModel>()
-//    val dummyQuestionAnswerModels = listOf(
-//        QuestionAnswerModel(
-//            questionerName = "김둘기",
-//            answerName = "둘기맘",
-//            questionContent = "오늘 점심은 무슨 음식을 먹었나요?",
-//            answerContent = "분식을 먹었어요! 맛있었어요!",
-//            answerImage = "https://avatars.githubusercontent.com/u/103172971?v=4",
-//        ),
-//        QuestionAnswerModel(
-//            questionerName = "둘기맘",
-//            answerName = "김둘기",
-//            questionContent = "영양제는 잘 챙겨먹고 있나요?",
-//            answerContent = "어제는 까먹었어요.. 오늘은 꼭 먹을게요!",
-//            answerImage = "https://avatars.githubusercontent.com/u/103172971?v=4",
-//        )
-//    )
+    val dummyQuestionAnswerModels = listOf(
+        QuestionAnswerModel(
+            questionerName = "김둘기",
+            answerName = "둘기맘",
+            questionContent = "오늘 점심은 무슨 음식을 먹었나요?",
+            answerContent = "분식을 먹었어요! 맛있었어요!",
+            answerImage = "https://avatars.githubusercontent.com/u/103172971?v=4",
+        ),
+        QuestionAnswerModel(
+            questionerName = "둘기맘",
+            answerName = "김둘기",
+            questionContent = "영양제는 잘 챙겨먹고 있나요?",
+            answerContent = "어제는 까먹었어요.. 오늘은 꼭 먹을게요!",
+            answerImage = "https://avatars.githubusercontent.com/u/103172971?v=4",
+        )
+    )
 }
--- feature/src/main/java/com/dongguk/telepigeon/feature/calendar/monthlyreport/MonthlyReportFragment.kt
+++ feature/src/main/java/com/dongguk/telepigeon/feature/calendar/monthlyreport/MonthlyReportFragment.kt
@@ -1,7 +1,43 @@
 package com.dongguk.telepigeon.feature.calendar.monthlyreport
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.dongguk.telepigeon.design.system.type.AppBarType
 import com.dongguk.telpigeon.core.ui.base.BindingFragment
 import com.dongguk.telepigeon.feature.databinding.FragmentMonthlyReportBinding
 
 class MonthlyReportFragment : BindingFragment<FragmentMonthlyReportBinding>({ FragmentMonthlyReportBinding.inflate(it) }) {
+    private val monthlyReportViewModel by viewModels<MonthlyReportViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAppBar()
+        initLayout()
+        setBtnMonthlyReportCheckClickListener()
+    }
+
+    private fun initAppBar() {
+        binding.appbarMonthlyReport.initLayout(appBarType = AppBarType.X)
+        binding.appbarMonthlyReport.binding.ivAppBarTelepigeonX.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun initLayout() {
+        with(binding) {
+            layoutMonthlyReportNegativeKeyword.visibility = View.INVISIBLE
+            layoutMonthlyReportPositiveKeyword.visibility = View.INVISIBLE
+            tvMonthlyReportPositiveKeyword.visibility = View.INVISIBLE
+            tvMonthlyReportNegativeKeyword.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setBtnMonthlyReportCheckClickListener() {
+        binding.btnMonthlyReportCheck.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
