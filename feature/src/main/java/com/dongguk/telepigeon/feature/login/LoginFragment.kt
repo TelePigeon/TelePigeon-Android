3678f5a97fa5685ac1f06aefddf148afe64f77fa
jihyunniiii <jita1125@naver.com>
2024-06-03 01:00:22 +0900

    [feat] #17 login

---
+++ feature/src/main/java/com/dongguk/telepigeon/feature/login/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.dongguk.telepigeon.feature.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.dongguk.telepigeon.feature.R
+import com.dongguk.telepigeon.feature.databinding.FragmentLoginBinding
+import com.dongguk.telpigeon.core.ui.base.BindingFragment
+
+class LoginFragment : BindingFragment<FragmentLoginBinding>({FragmentLoginBinding.inflate(it)}) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setLayoutLoginKakaoClickListener()
+    }
+
+    private fun setLayoutLoginKakaoClickListener() {
+        binding.layoutLoginKakao.setOnClickListener {
+            navigateToHome()
+        }
+    }
+
+    private fun navigateToHome() {
+        findNavController().navigate(R.id.action_login_to_home)
+    }
+}
