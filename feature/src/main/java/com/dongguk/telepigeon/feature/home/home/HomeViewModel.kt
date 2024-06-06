c79323205d63b523880cafdf8e4aad84e960b593
jihyunniiii <jita1125@naver.com>
2024-06-06 15:55:51 +0900

    [feat] #27 방 이동 시 방 id 저장 로직 구현

--- feature/src/main/java/com/dongguk/telepigeon/feature/home/home/HomeViewModel.kt
+++ feature/src/main/java/com/dongguk/telepigeon/feature/home/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dongguk.telepigeon.domain.model.HomeRoomModel
 import com.dongguk.telepigeon.domain.usecase.GetRoomsUseCase
+import com.dongguk.telepigeon.domain.usecase.SetRoomIdUseCase
 import com.dongguk.telpigeon.core.ui.util.view.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,21 +14,26 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel
-    @Inject
-    constructor(
-        private val getRoomsUseCase: GetRoomsUseCase,
-    ) : ViewModel() {
-        private val _getRoomsState = MutableStateFlow<UiState<List<HomeRoomModel>>>(UiState.Empty)
-        val getRoomsState get() = _getRoomsState.asStateFlow()
+@Inject
+constructor(
+    private val setRoomIdUseCase: SetRoomIdUseCase,
+    private val getRoomsUseCase: GetRoomsUseCase,
+) : ViewModel() {
+    private val _getRoomsState = MutableStateFlow<UiState<List<HomeRoomModel>>>(UiState.Empty)
+    val getRoomsState get() = _getRoomsState.asStateFlow()
 
-        fun getRooms() {
-            viewModelScope.launch {
-                _getRoomsState.value = UiState.Loading
-                getRoomsUseCase().onSuccess { homeRoomModels ->
-                    _getRoomsState.value = UiState.Success(homeRoomModels)
-                }.onFailure { exception: Throwable ->
-                    _getRoomsState.value = UiState.Error(exception.message)
-                }
+    fun getRooms() {
+        viewModelScope.launch {
+            _getRoomsState.value = UiState.Loading
+            getRoomsUseCase().onSuccess { homeRoomModels ->
+                _getRoomsState.value = UiState.Success(homeRoomModels)
+            }.onFailure { exception: Throwable ->
+                _getRoomsState.value = UiState.Error(exception.message)
             }
         }
     }
+
+    fun setRoomId(roomId: Int) {
+        setRoomIdUseCase(roomId = roomId)
+    }
+}
