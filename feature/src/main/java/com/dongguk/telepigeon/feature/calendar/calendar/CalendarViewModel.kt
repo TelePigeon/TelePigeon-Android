557084c14eaef9c8ade747b6c50120d4f7e24cc0
jihyunniiii <jita1125@naver.com>
2024-06-06 11:33:18 +0900

    [feat] #25 캘린더 뷰 질문-답변 확인 API 연동

--- feature/src/main/java/com/dongguk/telepigeon/feature/calendar/calendar/CalendarViewModel.kt
+++ feature/src/main/java/com/dongguk/telepigeon/feature/calendar/calendar/CalendarViewModel.kt
@@ -1,29 +1,37 @@
 package com.dongguk.telepigeon.feature.calendar.calendar
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dongguk.telepigeon.domain.model.QuestionAnswerModel
+import com.dongguk.telepigeon.domain.usecase.GetQuestionAnswerUseCase
+import com.dongguk.telepigeon.domain.usecase.GetRoomIdUseCase
+import com.dongguk.telpigeon.core.ui.util.view.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CalendarViewModel
-    @Inject
-    constructor() : ViewModel() {
-        val dummyQuestionAnswerModels =
-            listOf(
-                QuestionAnswerModel(
-                    questionName = "김둘기",
-                    answerName = "둘기맘",
-                    questionContent = "오늘 점심은 무슨 음식을 먹었나요?",
-                    answerContent = "분식을 먹었어요! 맛있었어요!",
-                    answerImage = "https://avatars.githubusercontent.com/u/103172971?v=4",
-                ),
-                QuestionAnswerModel(
-                    questionName = "둘기맘",
-                    answerName = "김둘기",
-                    questionContent = "영양제는 잘 챙겨먹고 있나요?",
-                    answerContent = "어제는 까먹었어요.. 오늘은 꼭 먹을게요!",
-                    answerImage = "https://avatars.githubusercontent.com/u/103172971?v=4",
-                ),
-            )
+@Inject
+constructor(
+    private val getRoomIdUseCase: GetRoomIdUseCase,
+    private val getQuestionAnswerUseCase: GetQuestionAnswerUseCase
+) : ViewModel() {
+    private val _getQuestionAnswerState = MutableStateFlow<UiState<List<QuestionAnswerModel>>>(UiState.Empty)
+    val getQuestionAnswerState get() = _getQuestionAnswerState.asStateFlow()
+
+    private val roomId = getRoomIdUseCase()
+
+    fun getQuestionAnswer(date: String) {
+        viewModelScope.launch {
+            _getQuestionAnswerState.value = UiState.Loading
+            getQuestionAnswerUseCase(roomId = roomId, date = date, respondent = false).onSuccess { questionAnswerModels ->
+                _getQuestionAnswerState.value = UiState.Success(questionAnswerModels)
+            }.onFailure { exception: Throwable ->
+                _getQuestionAnswerState.value = UiState.Error(exception.message)
+            }
+        }
     }
+}
