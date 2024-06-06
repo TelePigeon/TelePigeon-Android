f89aa0506bd775c5b6945178827727596d4d940f
jihyunniiii <jita1125@naver.com>
2024-06-06 17:35:32 +0900

    [feat] #30 걱정 추가 API 연동

--- feature/src/main/java/com/dongguk/telepigeon/feature/setting/addworry/AddWorryFragment.kt
+++ feature/src/main/java/com/dongguk/telepigeon/feature/setting/addworry/AddWorryFragment.kt
@@ -2,13 +2,22 @@ package com.dongguk.telepigeon.feature.setting.addworry
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.dongguk.telepigeon.design.system.component.BottomSheetTimeDialogFragment
 import com.dongguk.telepigeon.design.system.type.AppBarType
+import com.dongguk.telepigeon.domain.model.WorryModel
 import com.dongguk.telepigeon.feature.databinding.FragmentAddWorryBinding
 import com.dongguk.telpigeon.core.ui.base.BindingFragment
+import com.dongguk.telpigeon.core.ui.util.view.UiState
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class AddWorryFragment : BindingFragment<FragmentAddWorryBinding>({ FragmentAddWorryBinding.inflate(it) }) {
+    private val addWorryViewModel by viewModels<AddWorryViewModel>()
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
@@ -16,7 +25,21 @@ class AddWorryFragment : BindingFragment<FragmentAddWorryBinding>({ FragmentAddW
         super.onViewCreated(view, savedInstanceState)
 
         initLayout()
+        collectPostWorryState()
+        setBtnAddWorryAddClickListener()
         setEtAddWorryTimeClickListener()
+    }
+
+    private fun collectPostWorryState() {
+        addWorryViewModel.postWorryState.flowWithLifecycle(viewLifecycleOwner.lifecycle).onEach { postWorryState ->
+            when (postWorryState) {
+                is UiState.Success -> {
+                    findNavController().popBackStack()
+                }
+
+                else -> Unit
+            }
+        }.launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
     private fun initLayout() {
@@ -26,6 +49,17 @@ class AddWorryFragment : BindingFragment<FragmentAddWorryBinding>({ FragmentAddW
                 findNavController().popBackStack()
             }
             etAddWorryTime.editText.isEnabled = false
+        }
+    }
+
+
+    private fun setBtnAddWorryAddClickListener() {
+        binding.btnAddWorryAdd.setOnClickListener {
+            addWorryViewModel.postWorry(worryModel = WorryModel(
+                name = binding.etAddWorryName.editText.text.toString(),
+                content = binding.etAddWorryContent.editText.text.toString(),
+                times = binding.etAddWorryTime.editText.text.toString()
+            ))
         }
     }
 
